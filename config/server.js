c3a441855317893b3a8b8f70d6578ae01426d661
Allef Sousa <allefsousa_1@hotmail.com>
2017-09-08 22:30:27 -0300

    mudando o diretorio padrao das views

--- config/server.js
+++ config/server.js
@@ -8,6 +8,7 @@ var express = require ('express');
 var app = express();
 
 app.set('view engine','ejs');
+app.set('views','./app/views') // mudando o diretorio da View (Passando View e caminho)
 
 module.exports = app;
 
