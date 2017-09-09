5236f1636b780e36687d68828e29645eb3005abb
Allef Sousa <allefsousa_1@hotmail.com>
2017-09-09 16:01:41 -0300

    exibindodadosna View

---
+++ app/models/clienteModel.js
@@ -0,0 +1,18 @@
+
+/**
+ * trabalhando com modularização para organização do codigo
+ */
+
+module.exports = function(){
+
+    // this = faz referencia a toda a função.
+    // função para retornar um cliente
+    this.all = function(){
+        return[
+        {nome:"Allef",email:"allefsousa_1@hotmail.com"}
+        ];
+    };
+
+    return this;
+
+};
--- app/routes/web.js
+++ app/routes/web.js
@@ -1,12 +1,20 @@
+
+// adicionando o modulo de clientes  e ja executando a função com o () na frente;
+var cli = require("../models/clienteModel")();
+
 /**
  * Arquivo para configurar as rotas dentro diretorio route
  */
 module.exports = function(app){
   app.get('/contato',function(req,res){
+        console.log(cli.all()); // mostrando o nome do cliente
     res.render('site/contato');
   });
 
   app.get('/',function(req,res){
-    res.render('site/home');
+     //criando um objeto de clientes
+    var listaClientes = cli.all();
+
+    res.render('site/index',{c:listaClientes});
   });
 };
